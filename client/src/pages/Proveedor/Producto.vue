ed3905d156c548faf94af608e853b39ed4ff7f18
example <example@example.com>
2021-03-26 10:28:58 -0400

    h

--- client/src/pages/Proveedor/Producto.vue
+++ client/src/pages/Proveedor/Producto.vue
@@ -74,7 +74,7 @@
     <q-dialog v-model="alert.show">
       <q-card>
         <q-card-section>
-          <div class="text-h6">DATA CAMARA</div>
+          <div class="text-h6">DATA CAMARA QLQ ANDRES xD</div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
