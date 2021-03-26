e1afadd2e185c9a7c399ae63f14688161beea4d8
example <example@example.com>
2021-03-26 14:43:20 -0400

    sdf

--- client/src/pages/Proveedor/Producto.vue
+++ client/src/pages/Proveedor/Producto.vue
@@ -19,11 +19,7 @@
         <div class="column">
           <q-avatar size="80px" icon="add_a_photo" @click="captureImage" text-color="white" class="bg-primary">
           </q-avatar>
-          <q-img :src="test" style="width:100px;height:100px" /> 1
-          <q-img :src="test2" style="width:100px;height:100px" /> 2
-          <q-img :src="prueba.data" style="width:100px;height:100px" /> 3
-          <div> {{test}} </div>
-          <div> {{test2}} </div>
+          <q-img :src="prueba.data" style="width:100px;height:100px" />
           <div> {{prueba.data}} </div>
         </div>
       </div>
